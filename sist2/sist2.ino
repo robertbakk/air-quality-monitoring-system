3954fb5cfc3b4fdee0ed67d2ba06824269c23d39
robertbakk <61512299+robertbakk@users.noreply.github.com>
2021-01-07 06:37:30 +0200

    Add files via upload

--- sist2/sist2.ino
+++ sist2/sist2.ino
@@ -5,43 +5,47 @@
 #include "DHT.h"
 
 #define DHTPIN A3 //pinul la care este conectat senzorul DHT11
-#define DHTTYPE DHT11
+#define DHTTYPE DHT11 //tipul senzorului DHT
 #define MQ135PIN A0 //pinul la care este conectat senzorul MQ135
 #define MQ9PIN A1 //pinul la care este conectat senzorul MQ9
-#define MQ2PIN A2 //pinul la care este conectat senzorul MQ2
 
-#define Board ("Arduino UNO")
-#define MQ2Type ("MQ-2")
-#define MQ9Type ("MQ-9")
-#define Voltage_Resolution (5)
-#define ADC_Bit_Resolution (10)
-#define RatioMQ9CleanAir (9.6)
-#define RatioMQ2CleanAir (9.83)
+#define Board "Arduino UNO"
+#define MQ9Type "MQ-9"
+#define Voltage_Resolution 5
+#define ADC_Bit_Resolution 10
+#define RatioMQ9CleanAir 9.6 //ratia senzorului MQ in aer
 
-SoftwareSerial mySerial(3, 2);
-DHT dht(DHTPIN, DHTTYPE);
-MQ135 mq135 = MQ135(MQ135PIN);
-float h, t;
-float MQ9_LPG, MQ9_CH4, MQ9_CO, MQ135_CO2;
-int nrSecunde = 300;
-bool messageSent = false;
-MQUnifiedsensor MQ9(Board, Voltage_Resolution, ADC_Bit_Resolution, MQ9PIN, MQ9Type);
+#define NO_EMERGENCY -1 //nicio urgenta
+#define EMERGENCY_CO2 0 //nivel de urgenta pentru CO2
+#define EMERGENCY_CH4 1 //nivel de urgenta pentru CH4
+#define EMERGENCY_CO 2 //nivel de urgenta pentru CO
+#define NUMAR_TELEFON "+40746842759" //numarul de telefon pe care se primesc SMS-urile
+
+SoftwareSerial mySerial(3, 2); //RX, TX
+DHT dht(DHTPIN, DHTTYPE); // declarare senzor DHT11
+MQ135 mq135 = MQ135(MQ135PIN); //initializare senzor MQ135
+MQUnifiedsensor MQ9(Board, Voltage_Resolution, ADC_Bit_Resolution, MQ9PIN, MQ9Type); //initializare senzor MQ9
+float h, t; //umiditate si temperatura
+float MQ9_LPG, MQ9_CH4, MQ9_CO, MQ135_CO2; //valorile in ppm de LPG, CH4, CO si CO2
+volatile int nrSecunde[3] = {300, 300, 300}; //nr de secunde care au trecut de la ultimele SMS-uri cu un anumit nivel de urgenta
+                                            //nrSecunde[EMERGENCY_CO2], nrSecunde[EMERGENCY_CH4], nrSecunde[EMERGENCY_CO]
+String message; //mesajul care va fi trimis prin SMS
 
 void setup() {
-  Timer1.initialize(1000000);
-  Timer1.attachInterrupt(function);
-  dht.begin();
-  Serial.begin(9600);
-  mySerial.begin(9600);
+  Timer1.initialize(1000000); //se initializeaza un timer care are intrerupere la fiecare secunda
+  Timer1.attachInterrupt(function); //se ataseaza functia function timer-ului
+  pinMode(MQ9PIN, INPUT); //seteaza modul pinului MQ9PIN ca INPUT
+  pinMode(MQ135PIN, INPUT); //seteaza modul pinului MQ135PIN ca INPUT
+  dht.begin(); //initializare senzor DHT11
+  Serial.begin(9600); //initializare serial monitor
+  mySerial.begin(9600); //initializare software serial
 
-  MQ9.setRegressionMethod(1);
-  MQ9.init();
+  MQ9.setRegressionMethod(1); //functie exponentiala
 
   float calcR0MQ9 = 0;
   float calcR0MQ135 = 0;
 
-
-  
+  //se face calibrarea pentru senzorii MQ9 si MQ135
   for (int i = 0; i < 10; i++)
   {
     MQ9.update();
@@ -51,112 +55,101 @@ void setup() {
     calcR0MQ135 += mq135.getCorrectedRZero(t, h);
   }
 
-  MQ9.setR0(calcR0MQ9 / 10);
-  mq135.setR0(calcR0MQ135 / 10);
+  MQ9.setR0(calcR0MQ9 / 10); //se seteaza R0 pentru MQ9
+  mq135.setR0(calcR0MQ135 / 10); //se seteaza R0 pentru MQ135
 
+  //configurare software serial
   mySerial.println("AT");
   delay(200);
-  mySerial.println("AT+CMGF=1");
+  mySerial.println("AT+CMGF=1"); //configurare mod SMS TEXT
   delay(200);
   mySerial.println("AT+CNMI=1,2,0,0,0");
-  delay(200);
 }
 void loop() {
-  updateSerial();
+  checkForSMS();
   h = dht.readHumidity();
   t = dht.readTemperature();
 
-  MQ9.update();
+  MQ9.update(); //se face citirea de pe senzor
 
-  MQ9.setA(1000.5); MQ9.setB(-2.186);
-  MQ9_LPG = MQ9.readSensor();
+  //se seteaza parametrii a si b ai ecuatiei ppm = a * (RS/R0)^b
+  //RS = val. rezistentei in prezenta unui gaz (val. calculata in functie de val. citita de senzor, Voltage_Resolution, si ADC_Bit_Resolution)
+  //R0 = val. rezistentei in aerul curat
 
-  MQ9.setA(4269.6); MQ9.setB(-2.648);
-  MQ9_CH4 = MQ9.readSensor();
+  MQ9.setA(1000.5); MQ9.setB(-2.186); //parametrii ecuatiei pentru calculul LPG
+  MQ9_LPG = MQ9.readSensor(); //se interpreteaza valoarea citita de pe senzor cu parametrii setati pentru calculul ppm de LPG
 
-  MQ9.setA(599.65); MQ9.setB(-2.244);
-  MQ9_CO = MQ9.readSensor();
+  MQ9.setA(4269.6); MQ9.setB(-2.648); //parametrii ecuatiei pentru calculul CH4
+  MQ9_CH4 = MQ9.readSensor(); //se interpreteaza valoarea citita de pe senzor cu parametrii setati pentru calculul ppm de CH4
 
+  MQ9.setA(599.65); MQ9.setB(-2.244); //parametrii ecuatiei pentru calculul CO
+  MQ9_CO = MQ9.readSensor(); //se interpreteaza valoarea citita de pe senzor cu parametrii setati pentru calculul ppm de CO
+
+  //ppm de CO2 avand in vedere temperatura si umiditatea
   MQ135_CO2 = mq135.getCorrectedPPM(t, h);
+  delay(100);
 
 }
 
-void sendSMS(String message) {
-  if (nrSecunde > 300) {
-      mySerial.println("AT+CMGS=\"+40752173878\"");
-      delay(100);
-
-      mySerial.print(message);
-      delay(100);
-      mySerial.write(26);
-      Serial.println("MESSAGE SENT!");
-      nrSecunde = 0;
+//daca e urgenta, verifica sa fi trecut 300 de secunde de la ultimul SMS de urgenta
+void sendSMS(String message, int emergencyLevel) {
+  if (emergencyLevel != NO_EMERGENCY && nrSecunde[emergencyLevel] > 300) {
+    nrSecunde[emergencyLevel] = 0;
+    mySerial.println("AT+CMGS=\"" + String(NUMAR_TELEFON) + "\""); //configurarea numarului de telefon
+    delay(1000);
+    mySerial.print(message); //scrie mesajul in SMS
+    delay(100);
+    mySerial.write(26);
+    Serial.println("\nEMERGENCY MESSAGE SENT!");
+    delay(1000);
+  }
+  if (emergencyLevel == NO_EMERGENCY) {
+    mySerial.println("AT+CMGS=\"" + String(NUMAR_TELEFON) + "\""); //configurarea numarului de telefon
+    delay(1000);
+    mySerial.print(message); //scrie mesajul in SMS
+    delay(100);
+    mySerial.write(26);
+    Serial.println("\nSTATUS MESSAGE SENT!");
   }
 }
 
 void function() {
-  nrSecunde++;
+  nrSecunde[EMERGENCY_CH4]++; //creste nr de secunde care au trecut de la ultimul SMS trimis pentru nivelul de urgenta EMERGENCY_CH4
+  nrSecunde[EMERGENCY_CO]++; //creste nr de secunde care au trecut de la ultimul SMS trimis pentru nivelul de urgenta EMERGENCY_CO
+  nrSecunde[EMERGENCY_CO2]++; //creste nr de secunde care au trecut de la ultimul SMS trimis pentru nivelul de urgenta EMERGENCY_CO2
 
-  Serial.print("Temperature / Humidity: ");
-  Serial.print(t);
-  Serial.print("/");
-  Serial.print(h);
-  Serial.println("%");
+  //printeaza in Serial Monitor
+  Serial.println("\nTemperature / Humidity: " + String(t) + "/" + String(h) + "%");
+  Serial.println("MQ135 CO2: " + String(MQ135_CO2) + " ppm");
+  Serial.println("MQ9 LPG: " + String(MQ9_LPG) + "    CH4: " + String(MQ9_CH4) + "    CO:" + String(MQ9_CO));
 
-  Serial.print("MQ135 CO2: ");
-  Serial.print(MQ135_CO2);
-  Serial.println(" ppm");
-
-  Serial.print("MQ9 LPG: ");
-  Serial.print(MQ9_LPG);
-  Serial.print("    CH4: ");
-  Serial.print(MQ9_CH4);
-  Serial.print("    CO: ");
-  Serial.println(MQ9_CO);
-
+  //trimite SMS in caz de depasire nivel CH4
   if (MQ9_CH4 > 50)
-    sendSMS("Possible gas leak! - CH4 PPM: " + String(MQ9_CH4));
+    sendSMS("Possible gas leak! - CH4 PPM: " + String(MQ9_CH4), EMERGENCY_CH4);
 
+  //trimite SMS in caz de depasire nivel CO
   if (MQ9_CO > 150)
-    sendSMS("Possible fire! - CO PPM: " + String(MQ9_CO));
-  else {
-    if (MQ135_CO2 > 500)
-      sendSMS("You should open a window. - CO2 PPM: " + String(MQ135_CO2));
-  }
+    sendSMS("Possible fire! - CO PPM: " + String(MQ9_CO), EMERGENCY_CO);
+
+  //trimite SMS in caz de depasire nivel CO2
+  if (MQ135_CO2 > 500)
+    sendSMS("You should open a window. - CO2 PPM: " + String(MQ135_CO2), EMERGENCY_CO2);
 
 }
 
-void updateSerial()
+void checkForSMS()
 {
+  //verifica daca exista informatie primita ca SMS
   if (mySerial.available())
   {
-    String sms = mySerial.readString();
+    String sms = mySerial.readString(); //citeste SMS-ul primit
     Serial.println(sms);
-    if (sms.indexOf("status") > 0)
+    if (sms.indexOf("status") > 0) //verifica daca in SMS exista "status"
     {
-      mySerial.println("AT+CMGS=\"+40752173878\"");
-      delay(100);
-
-      mySerial.print("Temperature / Humidity: ");
-      mySerial.print(t);
-      mySerial.print("/");
-      mySerial.print(h);
-      mySerial.println("%");
-
-      mySerial.print("MQ135 CO2: ");
-      mySerial.print(MQ135_CO2);
-      mySerial.println(" ppm");
-
-      mySerial.print("MQ9 LPG: ");
-      mySerial.print(MQ9_LPG);
-      mySerial.print("    CH4: ");
-      mySerial.print(MQ9_CH4);
-      mySerial.print("    CO: ");
-      mySerial.println(MQ9_CO);
-      delay(100);
-      mySerial.write(26);
-      Serial.println("MESSAGE SENT!");
-
+      message = "Temperature / Humidity: " + String(t) + "/" + String(h) + "%\n"
+                + "MQ135 CO2: " + String(MQ135_CO2) + " ppm\n"
+                + "MQ9 LPG: " + String(MQ9_LPG) + "    CH4: " + String(MQ9_CH4) + "    CO: " + String(MQ9_CO);
+      sendSMS(message, NO_EMERGENCY); //trimite SMS cu statusul, fara nivel de urgenta
     }
   }
 }
